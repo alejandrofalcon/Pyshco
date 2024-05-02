b85e524c537c6ce9dea7740e6b814566a7326f44
alejandrofalcon <alejandrofalcon2001@gmail.com>
2024-05-02 16:13:12 +0200

    Ultima actualizacion

---
+++ app/src/main/java/com/example/pyshco/PantallaInicio.java
@@ -0,0 +1,15 @@
+package com.example.pyshco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PantallaInicio extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla_inicio);
+    }
+
+}
