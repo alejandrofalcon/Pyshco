1cbe088f3996eacf4cda2284b858c67c1bef0023
alejandrofalcon <alejandrofalcon2001@gmail.com>
2024-05-06 15:47:16 +0200

    Firebase configurado

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("com.android.application")
+    id("com.google.gms.google-services") version "4.4.1" apply false
+
+   
 }
+
 
 android {
     namespace = "com.example.pyshco"
@@ -36,9 +40,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.gms:play-services-ads:23.0.0")
-    implementation("androidx.navigation:navigation-fragment:2.6.0")
-    implementation("androidx.navigation:navigation-ui:2.6.0")
+
+    implementation(platform("com.google.firebase:firebase-bom:33.0.0"))
+    implementation("com.google.firebase:firebase-analytics")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/pyshco/MainActivity.java
+++ app/src/main/java/com/example/pyshco/MainActivity.java
@@ -1,20 +1,44 @@
 package com.example.pyshco;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText EmailLogin;
+    EditText passLogin;
+    Button btnLogin;
+    TextView TextRegistrarme;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    EmailLogin= findViewById(R.id.EmailLogin);
+    passLogin = findViewById(R.id.passLogin);
+    btnLogin = findViewById(R.id.btnLogin);
+    TextRegistrarme= findViewById(R.id.TextRegistrarme);
+
     }
+
+
+
     public void Registrarse (View view) {
         Intent intent = new Intent(this, Login.class);
         startActivity(intent);
     }
+    public void Inicio (View view) {
+        Intent intent = new Intent(this, Inicio.class);
+        startActivity(intent);
+    }
+
 }
