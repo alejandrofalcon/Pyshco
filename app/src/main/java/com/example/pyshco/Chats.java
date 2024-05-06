a56cdbfad5f8a78acf97f13644f6f9faef913aef
alejandrofalcon <alejandrofalcon2001@gmail.com>
2024-05-06 09:44:10 +0200

    Ultima actualizacions

--- app/src/main/java/com/example/pyshco/Chats.java
+++ app/src/main/java/com/example/pyshco/Chats.java
@@ -4,12 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PantallaInicio extends AppCompatActivity {
+public class Chats extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_pantalla_inicio);
+        setContentView(R.layout.activity_chats);
     }
-
 }
