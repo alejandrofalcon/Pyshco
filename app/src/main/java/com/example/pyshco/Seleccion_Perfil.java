5b99f2562b65f6b62ec9827878dbe34706133c9c
alejandrofalcon <alejandrofalcon2001@gmail.com>
2024-06-11 10:39:01 +0200

    Ultimo cambio

---
+++ app/src/main/java/com/example/pyshco/Seleccion_Perfil.java
@@ -0,0 +1,14 @@
+package com.example.pyshco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Seleccion_Perfil extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_perfil);
+    }
+}
